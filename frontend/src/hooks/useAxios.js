e225ca4caa67e0e3cac03ce1a6bf841c9dc55eeb
Juan-45 <quijuan12@gmail.com>
2023-05-19 19:50:43 -0300

    Update hook

--- frontend/src/hooks/useAxios.js
+++ frontend/src/hooks/useAxios.js
@@ -25,6 +25,10 @@ const useAxios = (method) => {
         .then(function (response) {
           console.log("Response---", response);
           //return response
+          return {
+            response: response,
+            error: false,
+          };
         })
         .catch(function (error) {
           //Component unmounted
@@ -32,12 +36,13 @@ const useAxios = (method) => {
             if (error.response) {
               // The request was made and the server responded with a status code
               // that falls out of the range of 2xx
-              console.log("Response error", error.response.data);
+              console.log("Response error", error.response);
               return {
-                title: "Ocurrió un error de servidor",
+                title: "Ocurrió un error de servidor.",
                 status: error.response.status,
                 code: error.response.data.code,
                 message: error.response.data.message,
+                url: error.response.request.responseURL,
                 error: true,
               };
             } else if (error.request) {
@@ -45,20 +50,22 @@ const useAxios = (method) => {
               // `error.request` is an instance of XMLHttpRequest in the browser
               console.log("No response error", error.request);
               return {
-                title: "La solicitud no obtuvo respuesta",
+                title: "La solicitud no obtuvo respuesta.",
                 status: error.request.status,
                 code: error.code,
                 message: error.message,
+                url: error.request.responseURL,
                 error: true,
               };
             } else {
               // Something happened in setting up the request that triggered an Error
               console.log("Request Error", error.message);
               return {
-                title: "La solicitud no pudo ser realizada",
+                title: "La solicitud no pudo ser realizada.",
                 status: "No definido",
                 code: error.code,
                 message: error.message,
+                url: error.config.url,
                 error: true,
               };
             }
