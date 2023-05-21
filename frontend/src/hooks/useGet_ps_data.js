e9d7c888efed6121437d218ad89956b60f5f0be8
Juan-45 <quijuan12@gmail.com>
2023-05-21 13:18:36 -0300

    Update useGet_ps_data

--- frontend/src/hooks/useGet_ps_data.js
+++ frontend/src/hooks/useGet_ps_data.js
@@ -8,21 +8,23 @@ const useGet_ps_data = ({ setContextState }) => {
   const requestGet_ps_data_id = useAxios("get");
 
   useEffect(() => {
-    const request_ps_data = async () => {
+    const request_ps_data = async (isUpdating) => {
       const ps_data_response = await requestGet_ps_data({
         url: "/psData",
       });
 
       if (ps_data_response.error) {
         console.log("Axios /psData error", ps_data_response);
-        setErrorData(ps_data_response);
+        if (isUpdating) {
+          setErrorData(ps_data_response);
+        } else setErrorData({ ...ps_data_response, shouldResetSite: true });
       } else {
         console.log("Axios /psData success", ps_data_response);
         setContextState((prevState) => ({
           ...prevState,
           ps_data: ps_data_response.response.data,
         }));
-        localStorage.setItem("items", JSON.stringify(items));
+        //localStorage.setItem("items", JSON.stringify(items));
       }
     };
 
@@ -38,7 +40,13 @@ const useGet_ps_data = ({ setContextState }) => {
         console.log("Axios /psDataid success", ps_data_id);
 
         if (false /*ps_data_id.data.id!==localstorage.ps_data.ps_data_id*/) {
-          request_ps_data();
+          request_ps_data(true);
+        } else {
+          /*capturar datos desde LS con funciónes de useLocalStorage, que serán recibidas como props para poder pasar un error de decompress a otro ErrorPopUp
+          si error durante descrompesion => borrar esos datos en LS y
+          llamar a request_ps_data(false)
+          
+          */
         }
       }
     };
@@ -46,7 +54,7 @@ const useGet_ps_data = ({ setContextState }) => {
     if (false /*localstorage.ps_data.ps_data_id === string */) {
       request_ps_data_id();
     } else {
-      request_ps_data();
+      request_ps_data(false);
     }
   }, [requestGet_ps_data, requestGet_ps_data_id, setContextState]);
 
