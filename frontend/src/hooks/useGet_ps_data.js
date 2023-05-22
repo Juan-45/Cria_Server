6495cb9a2a6b915fcbb9fb10ec006250d335d5fb
Juan-45 <quijuan12@gmail.com>
2023-05-22 08:16:10 -0300

    Update useget_ps_data for error management

--- frontend/src/hooks/useGet_ps_data.js
+++ frontend/src/hooks/useGet_ps_data.js
@@ -1,14 +1,14 @@
 import useAxios from "hooks/useAxios";
 import { useEffect, useState } from "react";
 
-const useGet_ps_data = ({ setContextState }) => {
+const useGet_ps_data = (setContextState, save_ps_data, load_ps_data) => {
   const [errorData, setErrorData] = useState({});
 
   const requestGet_ps_data = useAxios("get");
   const requestGet_ps_data_id = useAxios("get");
 
   useEffect(() => {
-    const request_ps_data = async (isUpdating) => {
+    const request_ps_data = async (isUpdating, onError = () => {}) => {
       const ps_data_response = await requestGet_ps_data({
         url: "/psData",
       });
@@ -17,18 +17,24 @@ const useGet_ps_data = ({ setContextState }) => {
         console.log("Axios /psData error", ps_data_response);
         if (isUpdating) {
           setErrorData(ps_data_response);
+          onError();
         } else setErrorData({ ...ps_data_response, shouldResetSite: true });
       } else {
         console.log("Axios /psData success", ps_data_response);
         setContextState((prevState) => ({
           ...prevState,
           ps_data: ps_data_response.response.data,
+          ps_data_ready: true,
         }));
-        //localStorage.setItem("items", JSON.stringify(items));
+        save_ps_data(ps_data_response.response.data);
       }
     };
 
-    const request_ps_data_id = async () => {
+    const request_ps_data_id = async (
+      current_ps_data_id,
+      onIdMatch = () => {},
+      onError = () => {}
+    ) => {
       const ps_data_id = await requestGet_ps_data_id({
         url: "/psDataid",
       });
@@ -36,23 +42,32 @@ const useGet_ps_data = ({ setContextState }) => {
       if (ps_data_id.error) {
         console.log("Axios /psDataid error", ps_data_id);
         setErrorData(ps_data_id);
+        onError();
       } else {
         console.log("Axios /psDataid success", ps_data_id);
-
-        if (false /*ps_data_id.data.id!==localstorage.ps_data.ps_data_id*/) {
-          request_ps_data(true);
+        if (ps_data_id.response.data.ps_data_id !== current_ps_data_id) {
+          request_ps_data(true, onError);
         } else {
-          /*capturar datos desde LS con funciónes de useLocalStorage, que serán recibidas como props para poder pasar un error de decompress a otro ErrorPopUp
-          si error durante descrompesion => borrar esos datos en LS y
-          llamar a request_ps_data(false)
-          
-          */
+          onIdMatch();
         }
       }
     };
 
-    if (false /*localstorage.ps_data.ps_data_id === string */) {
-      request_ps_data_id();
+    const current_ps_data_id = load_ps_data("ps_data_id");
+
+    if (current_ps_data_id) {
+      const callback = () => {
+        const ps_data = load_ps_data("ps_data");
+        if (ps_data) {
+          setContextState((prevState) => ({
+            ...prevState,
+            ps_data: ps_data,
+            ps_data_ready: true,
+          }));
+        }
+      };
+
+      request_ps_data_id(current_ps_data_id, callback, callback);
     } else {
       request_ps_data(false);
     }
