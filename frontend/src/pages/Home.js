9b2d397b248338d296383a3dd611e1e1d8eec5c0
Juan-45 <quijuan12@gmail.com>
2023-05-26 19:32:55 -0300

    Add functions for manual testing

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,37 @@
-const Home = () => {
-  return <h1>HOME</h1>;
+import {
+  saveItem,
+  getCurrentSessionTimestamp_key,
+  getCurrentSession_key,
+  getCurrentUserSessionID_key,
+  getLocalStorageSize,
+} from "helpers/localStorage";
+
+const Home = ({ currentUser }) => {
+  // boton para guardar datos en LS + id + timestamp-
+
+  const onClick = () => {
+    saveItem(
+      getCurrentSession_key(currentUser.id),
+      "DATOS_DE_LA_SESION_ACTUAL"
+    );
+    saveItem(
+      getCurrentSessionTimestamp_key(currentUser.id),
+      JSON.stringify(Date.now())
+    );
+    saveItem(
+      getCurrentUserSessionID_key(currentUser.id),
+      "ID_DE_LA_SESION_ACTUAL"
+    );
+    getLocalStorageSize();
+  };
+
+  return (
+    <>
+      <h1>HOME</h1>
+
+      <button onClick={onClick}>Guardar datos en LS</button>
+    </>
+  );
 };
 
 export default Home;
