623975a6ff0d88b69ac5055c66dfc90813ebd127
Juan-45 <quijuan12@gmail.com>
2023-05-26 19:29:24 -0300

    Add extra condition to render in PageRender

--- frontend/src/components/PageRender.js
+++ frontend/src/components/PageRender.js
@@ -1,11 +1,21 @@
+import { memo } from "react";
 import { Routes, Route } from "react-router-dom";
+import RenderIf from "components/RenderIf";
 
-const PageRender = ({ routesOptions }) => (
-  <Routes>
-    {routesOptions.map((item) => (
-      <Route key={item.path} path={item.path} element={item.element} />
-    ))}
-  </Routes>
-);
+const PageRender = ({ routesOptions, secondaryCondition }) => {
+  return (
+    <Routes>
+      {routesOptions.map((item) => (
+        <Route
+          key={item.path}
+          path={item.path}
+          element={
+            <RenderIf condition={secondaryCondition}>{item.element}</RenderIf>
+          }
+        />
+      ))}
+    </Routes>
+  );
+};
 
-export default PageRender;
+export default memo(PageRender);
