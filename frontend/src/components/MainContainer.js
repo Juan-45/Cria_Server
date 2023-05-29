e89a6422248dfcd098ca1bcd5d3168a5c6c9635a
Juan-45 <quijuan12@gmail.com>
2023-05-29 11:30:31 -0300

    Update MainContainer styles

--- frontend/src/components/MainContainer.js
+++ frontend/src/components/MainContainer.js
@@ -3,9 +3,9 @@ import { styled } from "@mui/material/styles";
 
 const MainContainer = styled(Box)(({ theme }) => ({
   minHeight: "100vh",
-  minWidth: "360px",
+  minWidth: "850px", //"360px",
   boxSizing: "border-box",
-  background: theme.palette.secondary.light,
+  background: theme.palette.background.default,
 }));
 
 export default MainContainer;
