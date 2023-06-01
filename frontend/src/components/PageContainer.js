bbb6972c64f0741ba1dd1b004068e2ea56ecdaec
Juan-45 <quijuan12@gmail.com>
2023-06-01 19:33:00 -0300

    Update PageContainer style

--- frontend/src/components/PageContainer.js
+++ frontend/src/components/PageContainer.js
@@ -3,25 +3,30 @@ import NavBar from "components/NavBar";
 import RenderIf from "components/RenderIf";
 import { styled } from "@mui/material/styles";
 
-const Container = styled(Box)({
+const Container = styled(Box, {
+  shouldForwardProp: (props) => props !== "topSpace",
+})(({ topSpace }) => ({
   height: "100%",
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
-});
+  paddingTop: topSpace ? "170px" : "0px",
+}));
 
 const PageContainer = ({
   children,
   navBarOptions,
   hideNavBar,
   handleManualClosing,
+  currentUser,
 }) => {
   return (
-    <Container>
+    <Container topSpace={!hideNavBar}>
       <RenderIf condition={!hideNavBar}>
         <NavBar
           navigationOptions={navBarOptions}
           handleManualClosing={handleManualClosing}
+          currentUser={currentUser}
         />
       </RenderIf>
       {children}
