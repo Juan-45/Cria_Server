7929fa36319538537797808183cddbda4d206843
Juan-45 <quijuan12@gmail.com>
2023-06-10 10:06:12 -0300

    Update combobox:
    - Styles
    - Logic

--- frontend/src/components/Combobox.js
+++ frontend/src/components/Combobox.js
@@ -1,4 +1,39 @@
-﻿import { TextField, Autocomplete } from "@mui/material";
+﻿import { useState } from "react";
+import { TextField, Autocomplete } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const StyledAutoComplete = styled(Autocomplete)(({ theme }) => ({
+  width: "100%",
+  marginBottom: theme.spacing(2),
+  color: theme.palette.text.secondary,
+  "& .MuiInputLabel-root.Mui-focused": {},
+  "& .MuiInputLabel-shrink": {
+    color: "inherit !important",
+    "&.Mui-error": {
+      color: `${theme.palette.error.main} !important`,
+    },
+  },
+  "& .MuiInput-input": {
+    padding: `${theme.spacing(0.5)} 0px ${theme.spacing(0.5)} 0px`,
+  },
+  "& .MuiAutocomplete-popupIndicator": {
+    color: theme.palette.grey[900],
+  },
+  "& .MuiAutocomplete-clearIndicator": {
+    color: theme.palette.grey[900],
+  },
+  "& .MuiTouchRipple-root": {
+    display: "none",
+  },
+  "& .MuiInputBase-root": {
+    "&::before": {
+      zIndex: 1,
+    },
+    "&::after": {
+      content: "unset",
+    },
+  },
+}));
 
 const Combobox = ({
   onChange,
@@ -11,22 +46,49 @@ const Combobox = ({
   options,
   ...props
 }) => {
+  const [oldValue, setOldValue] = useState({ val: "", id: "default" });
+
+  const getComboBoxValue = (value) => {
+    if (value) {
+      return {
+        val: value.val,
+        id: value.id,
+        adj: value.adj,
+        obj: value,
+      };
+    } else {
+      return {
+        val: "",
+        id: "default",
+        adj: "",
+        obj: { val: "", id: "default" },
+      };
+    }
+  };
+
+  const handleChange = (event, newValue) => {
+    const transformedValue = getComboBoxValue(newValue);
+    onChange(transformedValue);
+  };
+
   return (
-    <Autocomplete
+    <StyledAutoComplete
       value={value}
-      onChange={onChange}
+      onChange={handleChange}
       isOptionEqualToValue={(option, value) => option.id === value.id}
       getOptionLabel={(option) => option.val}
       //  autoSelect={true}
       openOnFocus={true}
       renderOption={(props, option) => {
-        return (
-          <li {...props} key={option.id}>
-            {option.val}
-          </li>
-        );
+        if (option.id !== "default") {
+          return (
+            <li {...props} key={option.id}>
+              {option.val}
+            </li>
+          );
+        }
       }}
-      options={options}
+      options={[...options, { val: "", id: "default" }]}
       {...props}
       renderInput={(params) => {
         const inputProps = params.inputProps;
